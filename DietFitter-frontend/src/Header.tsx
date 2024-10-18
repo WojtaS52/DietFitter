28f17f357b5110f93d3f3eb69feeac943ff0ab18
WojtaS52 <wojciechiderski@gmail.com>
2024-10-18 03:24:41 +0200

    initial commit of frontend part

---
+++ DietFitter-frontend/src/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>DietFitter</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/profile">Profile</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+
+    </header>
+  );
+}
+
+export default Header;
