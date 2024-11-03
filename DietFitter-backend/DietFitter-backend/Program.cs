e4d4a9bea2af4b1a559b1ee952153be97ec2f84a
WojtaS52 <wojciechiderski@gmail.com>
2024-11-03 23:13:50 +0100

    added postgres

--- DietFitter-backend/DietFitter-backend/Program.cs
+++ DietFitter-backend/DietFitter-backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -36,7 +38,9 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+
 app.Run();
+Console.WriteLine("Hello, World!");
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
