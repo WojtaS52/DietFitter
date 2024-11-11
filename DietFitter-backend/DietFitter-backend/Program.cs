3c5bbcf90f8a0de75ac1634f9c666d40d7f19774
WojtaS52 <wojciechiderski@gmail.com>
2024-11-11 19:06:58 +0100

    Added protected endpoint with authorization

--- DietFitter-backend/DietFitter-backend/Program.cs
+++ DietFitter-backend/DietFitter-backend/Program.cs
@@ -47,6 +47,7 @@
 //     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 // }
 
+using System.Security.Claims;
 using DietFitter_backend.Database;
 using DietFitter_backend.Extensions;
 using Microsoft.AspNetCore.Identity;
@@ -57,8 +58,17 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication();
-builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme)
+builder.Services.AddAuthorization();
+// builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme)
+//     .AddBearerToken(IdentityConstants.BearerScheme);
+
+//
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    })
+    .AddCookie(IdentityConstants.ApplicationScheme)
     .AddBearerToken(IdentityConstants.BearerScheme);
     
 
@@ -79,6 +89,16 @@ if (app.Environment.IsDevelopment())
     
     app.ApplyMigrations();
 }
+// Protected endpoint
+app.MapGet("users/me", async (ClaimsPrincipal claims, ApplicationDbContext context) =>
+    {
+        string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        
+        return await context.Users.FindAsync(userId);
+    }
+
+).RequireAuthorization();
+
 app.UseHttpsRedirection();
 app.MapIdentityApi<User>();
 app.Run();
